d44108464995fd02b7458aca5b90cd7aa5a486a6
Sai Gokula Krishnan <saigklkrishnan@gmail.com>
2024-03-25 03:27:54 +0530

    Updated url

--- Formula/fuckoff.rb
+++ Formula/fuckoff.rb
@@ -1,7 +1,7 @@
 class Fuckoff < Formula
   desc "A CLI tool to manage and clear Zsh history"
   homepage "https://github.com/Imgkl/fuckoff"
-  url "https://github.com/Imgkl/fuckoff-cli/archive/v1.0.0.tar.gz"
+  url  "https://github.com/Imgkl/fuckoff/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "883a25aec270d469bd310b68ab8a06e94dcec3d9a90502dd7c6d08b3c11db2e7"
 
   depends_on "python@3.9"
