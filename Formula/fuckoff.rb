a997a21ade95826af9dd1308a1c50379b8de4776
Sai Gokula Krishnan <saigklkrishnan@gmail.com>
2024-03-25 03:59:59 +0530

    trying virtual env

--- Formula/fuckoff.rb
+++ Formula/fuckoff.rb
@@ -9,10 +9,7 @@ class Fuckoff < Formula
   depends_on "python@3.9"
 
   def install
-    virtualenv_create(libexec, "python3.9")
-    system libexec/"bin/pip", "install", "questionary==2.0.1"
-    system libexec/"bin/pip", "install", "-e", "."
-    bin.install_symlink libexec/"bin/fuckoff"
+     virtualenv_install_with_resources
   end
 
   test do
