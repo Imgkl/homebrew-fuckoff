42b9fe0010a834e0ca1c38cf09669072a06a2479
Sai Gokula Krishnan <saigklkrishnan@gmail.com>
2024-03-25 03:48:24 +0530

    Updated formula indentation

--- Formula/fuckoff.rb
+++ Formula/fuckoff.rb
@@ -1,9 +1,9 @@
 class Fuckoff < Formula
-    include Language::Python::Virtualenv
+  include Language::Python::Virtualenv
 
-  desc "A CLI tool to manage and clear Zsh history"
+  desc "CLI tool to manage and clear Zsh history"
   homepage "https://github.com/Imgkl/fuckoff"
-  url  "https://github.com/Imgkl/fuckoff/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/Imgkl/fuckoff/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "883a25aec270d469bd310b68ab8a06e94dcec3d9a90502dd7c6d08b3c11db2e7"
 
   depends_on "python@3.9"
