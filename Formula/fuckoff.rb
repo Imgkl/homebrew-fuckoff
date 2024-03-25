50f3380c4843aa8deacb47938b9732d5802b8c63
Sai Gokula Krishnan <saigklkrishnan@gmail.com>
2024-03-25 08:22:12 +0530

    Updated Tap

--- Formula/fuckoff.rb
+++ Formula/fuckoff.rb
@@ -5,10 +5,10 @@ class Fuckoff < Formula
   homepage "https://github.com/Imgkl/fuckoff"
   url "https://github.com/Imgkl/fuckoff/archive/refs/tags/v1.0.3.tar.gz"
   sha256 "6158fcfe7caa8184fd8cde0671666b2b135da879ab734793d125fc1b52f352dc"
-
   depends_on "python@3.9"
 
   def install
+     system "pip", "install", "questionary==2.0.1"
      virtualenv_install_with_resources
   end
 
