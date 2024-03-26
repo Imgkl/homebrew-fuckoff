9f57e6f0869bb7027971c1c454dde02a0ff00fce
Sai Gokula Krishnan <saigklkrishnan@gmail.com>
2024-03-27 02:32:43 +0530

    Add prompt-toolkit dependency

--- Formula/fuckoff.rb
+++ Formula/fuckoff.rb
@@ -10,6 +10,7 @@ class Fuckoff < Formula
   def install
     venv = virtualenv_create(libexec, "python3.9", system_site_packages: true)
     venv.pip_install "questionary==2.0.1"
+    venv.pip_install "prompt-toolkit==3.0.36"
     venv.pip_install_and_link buildpath
     virtualenv_install_with_resources
   end
